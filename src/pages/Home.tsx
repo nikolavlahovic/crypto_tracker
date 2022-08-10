2bab2fb283e25f2a2d8ce5a12eb54b9c1ddd8760
Nikola Vlahovic <vlahovi.nikola@gmail.com>
2022-08-11 00:16:54 +0200

    subscription to multiple channels

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,38 +9,39 @@ type Currencies = {
   ethbtc: number[];
 };
 function Home() {
-  const [data, setData] = useState<Currencies | null>({
+  const [data, setData] = useState<Currencies>({
     btcusd: [0],
     ltcusd: [0],
     ltcbtc: [0],
     ethusd: [0],
     ethbtc: [0],
   });
-
-  const [topCurrencies, setTopCurrencies] = useState([
+  const [topCurrencies] = useState([
     "BTCUSD",
     "LTCUSD",
     "LTCBTC",
     "ETHUSD",
     "ETHBTC",
   ]);
-
   useEffect(() => {
     const ws = new WebSocket("wss://api-pub.bitfinex.com/ws/2");
-    ws.addEventListener("open", () => {
-      let msg = JSON.stringify({
-        event: "subscribe",
-        channel: "ticker",
-        pair: "BTCUSD",
+    topCurrencies.forEach((symbol) => {
+      ws.addEventListener("open", () => {
+        let msg = JSON.stringify({
+          event: "subscribe",
+          channel: "ticker",
+          pair: symbol.toUpperCase(),
+        });
+        ws.send(msg);
       });
-      ws.send(msg);
-    });
-
-    ws.addEventListener("message", (event) => {
-      if (JSON.parse(event.data)[1] !== "hb") {
-        setData(JSON.parse(event.data)[1]);
-        console.log(JSON.parse(event.data)[1]);
-      }
+      ws.addEventListener("message", (event) => {
+        if (JSON.parse(event.data)[1] !== "hb") {
+          setData((prevData) => ({
+            ...prevData,
+            [symbol.toLowerCase()]: JSON.parse(event.data)[1],
+          }));
+        }
+      });
     });
   }, [topCurrencies]);
   const headers = [
@@ -55,38 +56,64 @@ function Home() {
     if (symbol) return JSON.stringify(data) + symbol;
     return data;
   };
-  if (data) {
-    return (
-      <Table
-        headers={headers.map((header) => {
-          return (
-            <TableCell className={header.className}>{header.name}</TableCell>
-          );
-        })}
-      >
-        <tr>
-          <td>BTCUSD</td>
-          <TableCell className="right-cell">
-            {formatData(data.btcusd[6])}
-          </TableCell>
-          <TableCell className="right-cell">
-            {formatData(data.btcusd[4])}
-          </TableCell>
-          <TableCell className="right-cell">
-            {formatData(data.btcusd[5], "%")}
-          </TableCell>
-          <TableCell className="right-cell">
-            {formatData(data.btcusd[8])}
-          </TableCell>
-          <TableCell className="right-cell">
-            {formatData(data.btcusd[9])}
-          </TableCell>
-        </tr>
-      </Table>
-    );
-  } else {
-    return <p>problem</p>;
-  }
+  return data.btcusd ? (
+    <Table
+      headers={headers.map((header) => {
+        return (
+          <TableCell className={header.className}>{header.name}</TableCell>
+        );
+      })}
+    >
+      <tr>
+        <td>BTCUSD</td>
+        <TableCell className="right-cell">
+          {formatData(data.btcusd[6])}
+        </TableCell>
+        <TableCell className="right-cell">
+          {formatData(data.btcusd[4])}
+        </TableCell>
+        <TableCell className="right-cell">
+          {formatData(data.btcusd[5], "%")}
+        </TableCell>
+        <TableCell className="right-cell">
+          {formatData(data.btcusd[8])}
+        </TableCell>
+        <TableCell className="right-cell">
+          {formatData(data.btcusd[9])}
+        </TableCell>
+      </tr>
+      <tr>
+        <td>ltcusd</td>
+        <TableCell className="right-cell">
+          {formatData(data.ltcusd[6])}
+        </TableCell>
+        <TableCell className="right-cell">
+          {formatData(data.ltcusd[4])}
+        </TableCell>
+        <TableCell className="right-cell">
+          {formatData(data.ltcusd[5], "%")}
+        </TableCell>
+        <TableCell className="right-cell">
+          {formatData(data.ltcusd[8])}
+        </TableCell>
+        <TableCell className="right-cell">
+          {formatData(data.ltcusd[9])}
+        </TableCell>
+      </tr>
+    </Table>
+  ) : (
+    <Table
+      headers={headers.map((header) => {
+        return (
+          <TableCell className={header.className}>{header.name}</TableCell>
+        );
+      })}
+    >
+      <tr>
+        <td>BTCUSD</td>
+      </tr>
+    </Table>
+  );
 }
 
 export default Home;
